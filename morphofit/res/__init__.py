3c80dd7eaa39df153b97c884397446b9ac9c12a8
torluca <luca.t90@gmail.com>
2022-03-11 16:36:18 +0100

    changes to folder structure

---
+++ morphofit/res/__init__.py
@@ -0,0 +1,7 @@
+import pkg_resources
+
+__author__ = "Luca Tortorelli"
+__email__ = "Luca.Tortorelli@physik.lmu.de"
+__credits__ = "University Observatory, Ludwig-Maximilians-Universitaet Muenchen"
+
+__version__ = pkg_resources.require(__package__)[0].version
